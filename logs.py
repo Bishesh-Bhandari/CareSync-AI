271b9aa4a4be95fd80dd07d96afe9d4a844a8200
Bishesh-Bhandari <bisheshbhandari14@gmail.com>
2026-05-27 08:02:01 +0545

    learning fastapi

---
+++ logs.py
@@ -0,0 +1,7 @@
+patient_logs = []
+
+def add_log(log):
+    patient_logs.append(log)
+
+def get_logs():
+    return patient_logs
---
+++ main.py
@@ -0,0 +1,17 @@
+from logs import add_log, get_logs
+
+while True:
+
+    symptom = input("Enter symptom (or type exit): ")
+
+    if symptom.lower() == "exit":
+        break
+
+    add_log(symptom)
+
+all_logs = get_logs()
+
+print("\nPatient Logs:")
+
+for log in all_logs:
+    print("-", log)
